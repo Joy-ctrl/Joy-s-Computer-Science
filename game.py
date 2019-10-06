420a239dccc12e1e6c25d455352f0f2d4e289264
Mark Curtis <mark@BigMac.local>
2019-10-06 20:08:13 +0100

    Updated for some basic gameplay

--- game.py
+++ game.py
@@ -8,10 +8,10 @@ import util
 # making sure we dont choose the same types
 def choose_pokemon(name):
      global user_pokemon
-     global opp_pokemon 
+     global cpu_pokemon 
      user_pokemon = pokemon(name)
-     opp_name = util.choose_opp(name)
-     opp_pokemon = pokemon(opp_name)
+     cpu_name = util.choose_opp(name)
+     cpu_pokemon = pokemon(cpu_name)
 
 # function to print out message and return input
 def prompt(message):
@@ -38,12 +38,50 @@ def main():
         print("No valid choice made, please try again.")
         exit()
     
-    # Create the user and opposing pokemon
+    # Create the user and cpuosing pokemon
     choose_pokemon(chosen)
     
     # Just for debugging
     debug()
+
+    # Start the battle
+    battle()
+
+# This is where the battle happens    
+def battle():
+    # Start the battle
+    print(user_pokemon.name + " will battle " + cpu_pokemon.name + "!")
+    # Prints the battle status
+    status()
+
+    battle_turn = 1
+    while user_pokemon.health > 0 and cpu_pokemon.health > 0:
+        if battle_turn == 1:
+            prompt("Your turn to attack, hit enter...")
+            cpu_pokemon.health = cpu_pokemon.health - user_pokemon.attack + cpu_pokemon.defence
+            battle_turn = 2
+        else:
+            prompt("CPUs turn to attach, hit enter...")
+            user_pokemon.health = user_pokemon.health - cpu_pokemon.attack + user_pokemon.defence
+            battle_turn = 1
+        status()
     
+    # Declare winner
+    if user_pokemon.health > 0 and cpu_pokemon.health <= 0:
+        print ">>> The winner is: " + user_pokemon.name + "!!! <<<"
+    elif cpu_pokemon.health > 0 and user_pokemon.health <= 0:
+        print ">>> The winner is: " + cpu_pokemon.name + "!!! <<<"
+    else:
+        print ">>> Its a draw!!! <<<"
+
+# Prints game status
+def status():
+    print "*** " + user_pokemon.name + " health: " + str(user_pokemon.health)
+    print "*** " + cpu_pokemon.name + " health: " + str(cpu_pokemon.health)
+
+
+
+
 # fucntion to just print out pokemon attributes
 def debug():
     print "Your pokemon: " + str(user_pokemon.name)
@@ -51,8 +89,8 @@ def debug():
     for att in  attrs.items():
         print att
 
-    print "CPU pokemon: " + str(opp_pokemon.name)
-    attrs = vars(opp_pokemon)
+    print "CPU pokemon: " + str(cpu_pokemon.name)
+    attrs = vars(cpu_pokemon)
     for att in  attrs.items():
         print att
 
--- util.py
+++ util.py
@@ -8,9 +8,18 @@ import random
 class pokemon:
     def __init__(self, name):
         self.name=name
-        self.health=genrandom(1,10)
-        self.attack=genrandom(1,10)
-        self.defence=genrandom(1,10)
+        self.health=genrandom(50,100)
+        self.attack=genrandom(11,20)
+        self.defence=genrandom(5,10)
+    
+    def change_health(self, num):
+        self.health = self.heatlh + int(num)
+
+    def change_attack(self, num):
+        self.attack = self.attack + int(num)
+
+    def change_defence(self, num):
+        self.defence = self.defence + int(num)
 
 # Takes number min, max and generates a random
 # number in that range
@@ -19,7 +28,7 @@ def genrandom(min, max):
    return rndint
 
 # Ensures opposing pokemon
-# is different
+# are different
 def choose_opp(name):
    if name == "Charmander":
        opp_name = "Squirtle"
@@ -30,5 +39,4 @@ def choose_opp(name):
    else:
        opp_name = "Unknown"
    return opp_name
-
 
